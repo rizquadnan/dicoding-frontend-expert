cf3af07daf583dea9b87eb7ced1935a1acf52961
Adnan <muhammad.adnan.rizqu@gmail.com>
2021-05-04 13:00:01 +0700

    done optimization - off cancas menu + text width

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles/main.css';
+
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#drawer");
+
+hamburgerButtonElement.addEventListener("click", event => {
+  drawerElement.classList.toggle("open");
+  event.stopPropagation();
+ });
+
+ const mainElement = document.querySelector("main");
+
+ mainElement.addEventListener("click", event => {
+  drawerElement.classList.remove("open");
+  event.stopPropagation();
+ })
